47fd7456470bb9fae19e534371477dc4d707bfbf
mariaclaramussi <35239895+mariaclaramussi@users.noreply.github.com>
2018-02-28 19:36:32 -0300

    Complementando slides e pagina de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,6 +13,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
     this.conteudos = [{nome: 'Avisos Gerais', desc: 'Geral Geral'}, {nome: 'Presidência', desc: 'presidência Presidência'}, {nome: 'Marketing', desc: 'marketing marketing'}, {nome: 'Projeto', desc: 'projeto projeto'},{nome: 'GP', desc: 'GP GP GP'}, {nome: 'Financeiro', desc: 'financeiro financeiro'}]; 
   }
+  
   navega(){
     this.navCtrl.push("NovoAvisoPage");
   }
