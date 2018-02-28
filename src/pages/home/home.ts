6f46ed4b820e2e970acd7b1aeece108b235a09a9
Beatriz Tantow <beatriz.tantow@gmail.com>
2018-02-28 14:46:06 -0300

    Criação da página novo aviso.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
+export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  navega(){
+    this.navCtrl.push("NovoAvisoPage");
+  }
 }
